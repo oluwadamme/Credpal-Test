ba4e0296a0d31151c8cd81876a488670b1101909
Damilola Adeniyi <adeniiyidamilola246@gmail.com>
2024-01-24 12:50:59 +0100

    design the product ui

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -101,13 +101,115 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           Container(
-            padding: const EdgeInsets.all(20),
+            padding: const EdgeInsets.symmetric(horizontal: 21, vertical: 14),
             color: const Color(0xFFF1F3FE),
+            width: MediaQuery.of(context).size.width,
             child: Column(
-              children: [],
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 140,
+                  width: MediaQuery.of(context).size.width,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) => const ProductCard(),
+                    separatorBuilder: (context, index) => const SizedBox(width: 24),
+                    itemCount: 4,
+                  ),
+                ),
+                const SizedBox(height: 26),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 140,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) => const ProductCard(),
+                    separatorBuilder: (context, index) => const SizedBox(width: 24),
+                    itemCount: 4,
+                  ),
+                ),
+              ],
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      color: Colors.white,
+      shadowColor: const Color(0x0C000000),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+        ),
+        constraints: const BoxConstraints(maxWidth: 161),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Image.asset(
+                    "assets/images/Product Image.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                Text(
+                  'iPhone XS Max 4GBbe',
+                  style: blackStyle(14, Colors.black),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    Text('₦ 39,780', style: blackStyle(14, const Color(0xFF274FED), family: "")),
+                    const SizedBox(width: 14),
+                    Text('₦ 88,000',
+                        style: mediumStyle(12, const Color(0xFFB2B2CB),
+                            family: '', decoration: TextDecoration.lineThrough))
+                  ],
+                )
+              ],
+            ),
+            Container(
+              width: 50,
+              height: 50,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 7),
+              decoration: const ShapeDecoration(
+                color: Colors.white,
+                shape: OvalBorder(),
+                shadows: [
+                  BoxShadow(
+                    color: Color(0x0C000000),
+                    blurRadius: 10,
+                    offset: Offset(0, 4),
+                    spreadRadius: 0,
+                  )
+                ],
+              ),
+              child: Image.asset(
+                "assets/images/Pay40.png",
+                fit: BoxFit.scaleDown,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
